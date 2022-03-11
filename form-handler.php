4ab062ed0a92615521cf20e690fbf491509a0099
arnav-nirankari <96484556+arnav-nirankari@users.noreply.github.com>
2022-03-11 11:48:29 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -3,15 +3,15 @@ $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $phone = $_POST['phone'];
 
-$email_from = 'arnav.nirankari@student.ufv.ca';
+$email_from = 'Arnav.Nirankari@student.ufv.ca';
 
 $email_subject = 'New Form Submission';
 
 $email_body = "User Name: $name.\n".
                "User Email: $visitor_email.\n".
-               "User Phone: $phone.\n".;
+               "User Phone: $phone.\n";
 
-$to = 'arnav.nirankari@student.ufv.ca';
+$to = 'Arnav.Nirankari@student.ufv.ca';
 
 $headers = "From: $email_from \r\n";
 
@@ -19,5 +19,5 @@ $headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to, $email_subject,$email_body,$headers);
 
-header("Location: index.hmtl");
+header("Location: index.html");
 ?>
